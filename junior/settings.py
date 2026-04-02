5a39404e56452c312e195614b5132a33ba28fa44
Juliveson <juliveson26@gmail.com>
2026-04-02 14:38:20 +0300

    updated database

--- junior/settings.py
+++ junior/settings.py
@@ -75,11 +75,19 @@ WSGI_APPLICATION = 'junior.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
